94af5bc92e7f58f7d2cb308728abb3b50b799ffb
kemal Celikel <kemal409@hotmail.com>
2025-08-27 14:09:07 +0300

    use version.py for versioning

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+from version import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="ipio",
-    version="26.2876.2308",
+    version=__version__,
     author="Kemal Çelikel",
     author_email="kcelikel@eventgates.com",
     description="IPIO Python Library",
